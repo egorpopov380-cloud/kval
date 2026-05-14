9171e6436c4518948723e54a67d994601bc36898
egorpopov380-cloud <egorpopov380@gmail.com>
2026-05-14 14:13:28 +0400

    Update postgresql.js

--- src/db/postgresql.js
+++ src/db/postgresql.js
@@ -8,7 +8,10 @@ const connect = {
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS
+    password: process.env.DB_PASS,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 }
 
 const DB = pg(connect);
